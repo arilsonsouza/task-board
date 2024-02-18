756ea5ef3a4c1b1244dda0226c03e841b0136915
Arilson Souza <arilsons61@gmail.com>
2024-02-18 12:54:10 -0300

    feat(frontend): add default theme

---
+++ frontend/src/styles/themes/default.ts
@@ -0,0 +1,18 @@
+export const defaultTheme = {
+  colors: {
+    white: '#FFFFFF',
+    black: '#000000',
+    transparentBlack: '#00000033',
+    winterWhite: '#F8FAFC',
+    sunshineYellow: '#F5D565',
+    vanillaCream: '#F5E8D5',
+    caramelGold: '#E9A23B',
+    springBreezeGreen: '#A0ECB1',
+    electricGreen: '#32D657',
+    blushPink: '#F7D4D3',
+    rubyRed: '#DD524C',
+    paleSkyBlue: '#E3E8EF',
+    duskBlue: '#97A3B6',
+    sapphireBlue: '#3662E3'
+  }
+} as const
