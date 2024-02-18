b952b1ed7ab285886271037be2915a366e1547c1
Arilson Souza <arilsons61@gmail.com>
2024-02-17 21:46:37 -0300

    feat: add task repository

---
+++ backend/src/main/java/com/aos/taskboard/repositories/TaskRepository.java
@@ -0,0 +1,16 @@
+package com.aos.taskboard.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.aos.taskboard.domain.task.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+  @Query("SELECT t FROM Task t WHERE t.user.id = :user_id ORDER BY t.createdAt")
+  public List<Task> findAllByUserId(@Param("user_id") Long userId);
+}
