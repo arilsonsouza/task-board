bb952cbf30cc561b9799c686db5bdc1477ae195b
Arilson Souza <arilsons61@gmail.com>
2024-02-24 12:55:29 -0300

    feat: update authentication payload validation

--- backend/src/main/java/com/aos/taskboard/domain/user/DTO/AuthenticationRequestDTO.java
+++ backend/src/main/java/com/aos/taskboard/domain/user/DTO/AuthenticationRequestDTO.java
@@ -1,5 +1,9 @@
 package com.aos.taskboard.domain.user.DTO;
 
-public record AuthenticationRequestDTO(String email, String password) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+public record AuthenticationRequestDTO(@NotBlank @Email String email, @NotBlank @Min(6) String password) {
 
 }
--- backend/src/main/java/com/aos/taskboard/domain/user/DTO/RegisterDTO.java
+++ backend/src/main/java/com/aos/taskboard/domain/user/DTO/RegisterDTO.java
@@ -1,5 +1,13 @@
 package com.aos.taskboard.domain.user.DTO;
 
-public record RegisterDTO(String username, String email, String password) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+public record RegisterDTO(
+    @NotBlank @Min(3) @Max(20) String username,
+    @NotBlank @Email String email,
+    @NotBlank @Min(6) String password) {
 
 }
