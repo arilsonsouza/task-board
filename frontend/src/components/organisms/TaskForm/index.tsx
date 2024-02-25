3dfd39501728bf4c38944709a4e03f6ba6172f84
Arilson Souza <arilsons61@gmail.com>
2024-02-25 15:37:35 -0300

    feat: add TaskForm component

---
+++ frontend/src/components/organisms/TaskForm/index.tsx
@@ -0,0 +1,192 @@
+import { useEffect } from 'react'
+import { useForm, Controller } from 'react-hook-form'
+import { useContextSelector } from 'use-context-selector'
+import * as z from 'zod'
+
+import { FormField } from "../../molecules/FormField"
+import { zodResolver } from '@hookform/resolvers/zod'
+import { FormTextField } from '../../molecules/FormTextField'
+import { TasksContext } from '../../../contexts/TasksContext'
+import { CanceledTaskIcon } from '../../atoms/CanceledTaskIcon'
+import { CompletedTaskIcon } from '../../atoms/CompletedTaskIcon'
+import { InProgressTaskIcon } from '../../atoms/InProgressTaskIcon'
+import saveTaskButtonIcon from '../../../assets/images/Done_round.svg'
+import { TaskIconInputField } from '../../molecules/TaskIconInputField'
+import { TaskStatusInputField } from '../../molecules/TaskStatusInputField'
+
+import { SaveTaskButton } from './styles'
+
+const taskFormSchema = z.object({
+  title: z.string().trim().min(1, { message: 'Task title is required' }).max(40),
+  description: z.string().trim().min(1, { message: 'Task description is required' }).max(40),
+  icon: z.enum(["🧑🏼‍💻", "💬", "☕", "🏋️", "📚", "⏰"]),
+  status: z.enum(['IN_PROGRESS', 'COMPLETED', 'CANCELED']),
+})
+
+export type TaskFormInputs = z.infer<typeof taskFormSchema>
+
+export function TaskForm() {
+  const { task, saveTask } = useContextSelector(TasksContext, (context) => {
+    return {
+      task: context.task,
+      saveTask: context.saveTask,
+    }
+  })
+
+  const { title, description, status, icon } = task
+
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors, },
+    watch,
+    reset
+  } = useForm<TaskFormInputs>({
+    resolver: zodResolver(taskFormSchema),
+  })
+
+  useEffect(() => {
+    reset({
+      title: title,
+      description: description,
+      status: status,
+      icon: icon
+    })
+  }, [title, description, status, reset, icon])
+
+  const selectedStatus = watch("status")
+  const selectedIcon = watch("icon")
+
+  async function handleFormSubmit(formData: TaskFormInputs) {
+    const success = await saveTask(formData)
+    if (success) {
+      reset({
+        title: "",
+        description: "",
+        status: "IN_PROGRESS",
+        icon: "🧑🏼‍💻"
+      })
+    }
+  }
+
+  return (
+    <form
+      className="flex flex-col gap-1 mx-auto w-full"
+      onSubmit={handleSubmit(handleFormSubmit)}
+    >
+      <FormField
+        label="Task name"
+        type="text"
+        placeholder="Task title"
+        required
+        defaultValue={title}
+        {...register('title')}
+        error={errors.title?.message}
+      />
+
+      <FormTextField
+        label="Description"
+        placeholder="Enter a short description"
+        required
+        defaultValue={description}
+        {...register('description')}
+        error={errors.description?.message}
+      />
+
+      <Controller
+        control={control}
+        name="icon"
+        defaultValue={icon}
+        render={({ field }) =>
+          <div className="flex flex-col mt-1">
+            <span className="label-text">Icon</span>
+            <div className="flex gap-2">
+              <TaskIconInputField
+                {...field}
+                value="🧑🏼‍💻"
+                checked={selectedIcon == "🧑🏼‍💻"}
+                icon="🧑🏼‍💻"
+              />
+              <TaskIconInputField
+                {...field}
+                value="💬"
+                checked={selectedIcon == "💬"}
+                icon="💬"
+              />
+              <TaskIconInputField
+                {...field}
+                value="☕"
+                checked={selectedIcon == "☕"}
+                icon="☕"
+              />
+              <TaskIconInputField
+                {...field}
+                value="🏋️"
+                checked={selectedIcon == "🏋️"}
+                icon="🏋️"
+              />
+              <TaskIconInputField
+                {...field}
+                value="📚"
+                checked={selectedIcon == "📚"}
+                icon="📚"
+              />
+              <TaskIconInputField
+                {...field}
+                value="⏰"
+                checked={selectedIcon == "⏰"}
+                icon="⏰"
+              />
+            </div>
+          </div>
+        }
+      />
+
+      <Controller
+        control={control}
+        name="status"
+        defaultValue={status}
+        render={({ field }) =>
+          <div className="flex flex-col mt-1">
+            <span className="label-text">Status</span>
+            <div className="flex flex-wrap gap-3">
+              <TaskStatusInputField
+                {...field}
+                value="IN_PROGRESS"
+                label='In Progress'
+                checked={selectedStatus == "IN_PROGRESS"}
+                icon={<InProgressTaskIcon />}
+              />
+              <TaskStatusInputField
+                {...field}
+                value="COMPLETED"
+                label='Completed'
+                checked={selectedStatus == "COMPLETED"}
+                icon={<CompletedTaskIcon />}
+              />
+
+              <TaskStatusInputField
+                {...field}
+                value="CANCELED"
+                label="Won't do"
+                checked={selectedStatus == "CANCELED"}
+                icon={<CanceledTaskIcon />}
+              />
+            </div>
+          </div>
+        }
+      />
+
+      <div className="flex justify-end gap-4 mt-3">
+        <SaveTaskButton
+          type='submit'
+          disabled={isSubmitting}
+          className="flex items-center gap-2 py-2 px-6 text-white rounded-3xl text-base">
+          <span>Save</span>
+          <img src={saveTaskButtonIcon} alt="Save task" />
+        </SaveTaskButton>
+      </div>
+    </form>
+  )
+}
---
+++ frontend/src/components/organisms/TaskForm/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const SaveTaskButton = styled.button`
+  background-color: ${({ theme }) => theme.colors.sapphireBlue};
+
+  &:not(:disabled):hover {
+    opacity: .9;
+    transition: opacity 0.2s;
+  }
+
+  &:disabled {
+    opacity: .7;
+    cursor: not-allowed;
+  }
+`
